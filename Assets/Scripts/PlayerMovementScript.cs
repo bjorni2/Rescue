197679c72f24a7b153ccdc19b73720c5ffbea131
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-12-23 00:33:22 +0000

    collider size changed when crawling

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -8,6 +8,7 @@ public class PlayerMovementScript : MonoBehaviour {
 
 	public bool canHide = false;
 	public bool isHidden = false;
+	public bool isCrawling = false;
 
 	public float maxSpeed;
 
@@ -46,6 +47,10 @@ public class PlayerMovementScript : MonoBehaviour {
 
 	void MovePlayer (bool direction)
 	{
+		if(isCrawling){
+			enlargeHitbox();
+		}
+
 		isHidden = false;
 		if(direction == RIGHT)
 		{
@@ -71,7 +76,8 @@ public class PlayerMovementScript : MonoBehaviour {
 		{
 			anim.SetTrigger ("Crawling");
 			isHidden = false;
-			//GetComponent<SpriteRenderer>().color = Color.green;
+
+			shrinkHitbox();
 		}
 
 		rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
@@ -79,9 +85,28 @@ public class PlayerMovementScript : MonoBehaviour {
 
 	void StandStill ()
 	{
+		if(isCrawling){
+			enlargeHitbox();
+		}
+
 		isHidden = false;
 		GetComponent<SpriteRenderer>().color = new Color(1.0f, 235.0f/255.0f, 4.0f/255.0f, 1.0f);
 		rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
 	}
 
+	void enlargeHitbox()
+	{
+		isCrawling = false;
+		BoxCollider2D collider = GetComponent<BoxCollider2D> ();
+		collider.size = new Vector2 (0.64f, 1.28f);
+		collider.center = new Vector2 (0.0f, 0.0f);
+	}
+
+	void shrinkHitbox()
+	{
+		isCrawling = true;
+		BoxCollider2D collider = GetComponent<BoxCollider2D> ();
+		collider.size = new Vector2 (0.64f, 0.64f);
+		collider.center = new Vector2 (0.0f, -0.32f);
+	}
 }
